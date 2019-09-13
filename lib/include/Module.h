a91b478ea4787cb1ab985278c3bd78f9781f8a11
Fabian Wahlster <razor8@gmx.de>
2019-09-13 17:24:28 +0200

    module capabilities

--- lib/include/Module.h
+++ lib/include/Module.h
@@ -15,6 +15,13 @@ namespace spvgentwo
 
 		Function& addFunction() { return emplace_back(this); }
 
+		void addCapability(const spv::Capability _capability);
+
+		const List<Instruction>& getCapabilities() const { return m_Capabilities; }
+
 	private:
+
+		// preamble
+		List<Instruction> m_Capabilities;
 	};
 } // !spvgentwo
--- lib/source/Module.cpp
+++ lib/source/Module.cpp
@@ -1,13 +1,17 @@
 #include "Module.h"
 #include "Function.h"
 
-//using namespace spvgentwo;
-
 spvgentwo::Module::Module(IAllocator* _pAllocator) :
-	List(_pAllocator)
+	List(_pAllocator), m_Capabilities(_pAllocator)
 {
 }
 
 spvgentwo::Module::~Module()
 {
+}
+
+void spvgentwo::Module::addCapability(const spv::Capability _capability)
+{
+	// emplace free instruction (without parent BB)
+	m_Capabilities.emplace_back(m_pAllocator).opCapability(_capability);
 }
