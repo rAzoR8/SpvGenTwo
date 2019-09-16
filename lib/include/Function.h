f62439d80d9085dc572bff4879c75ad242f974d4
Fabian Wahlster <razor8@gmx.de>
2019-09-16 14:54:48 +0200

    function signature utilities

--- lib/include/Function.h
+++ lib/include/Function.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "BasicBlock.h"
+#include "Type.h"
 
 namespace spvgentwo
 {
@@ -23,10 +24,48 @@ namespace spvgentwo
 		const Module* getModule() const { return m_pModule; }
 
 		void write(IWriter* _pWriter) const;
+
+		// calls finalize internally (all-in-one function)
+		template <class ... Types>
+		void finalizeSignature(const Flag<spv::FunctionControlMask> _control, const Type& _returnType, Types&& ... _args);
+
+		const List<Instruction>& getParameters() const { return m_Parameters; }
+
+		// returns the OpTypeFunction, finalize needs to be called
+		Type& createSignature();
+
+		bool finalize(const Flag<spv::FunctionControlMask> _control = spv::FunctionControlMask::MaskNone);
+
+		template <class ... Types>
+		void addParameters(const Type& _type, const Types& ... _args);
+
 	private:
 		Module* m_pModule = nullptr; // parent
 
-		Instruction m_Function;
-		List<Instruction> m_Parameters;
+		Instruction m_Function; // OpFunction
+		const Instruction* m_pFunctionType = nullptr;
+		Type m_Type; // OpFunctionType
+
+		List<Instruction> m_Parameters; // OpFunctionParameters
 	};
+
+	template<class ...Types>
+	inline void Function::finalizeSignature(const Flag<spv::FunctionControlMask> _control, const Type& _returnType, Types&& ... _args)
+	{
+		addParameters(_returnType, _args...);
+
+		finalize(_control);
+	}
+
+	template<class ...Types>
+	inline void Function::addParameters(const Type& _type, const Types& ..._args)
+	{
+		m_Type.getSubTypes().emplace_back(_type);
+
+		if constexpr (sizeof...(_args) > 0)
+		{
+			addParameters(_args...);
+		}
+	}
+
 } // !spvgentwo
--- lib/source/Function.cpp
+++ lib/source/Function.cpp
@@ -4,17 +4,17 @@
 spvgentwo::Function::Function(IAllocator* _pAllocator) :
 	m_pModule(nullptr), List(_pAllocator),
 	m_Function(_pAllocator),
-	m_Parameters(_pAllocator)
+	m_Parameters(_pAllocator),
+	m_Type(_pAllocator)
 {
-	//m_Function.opFunction()
 }
 
 spvgentwo::Function::Function(Module* _pModule) :
 	m_pModule(_pModule), List(_pModule->getAllocator()),
 	m_Function(_pModule->getAllocator()),
-	m_Parameters(_pModule->getAllocator())
+	m_Parameters(_pModule->getAllocator()),
+	m_Type(_pModule->getAllocator())
 {
-	//m_Function.opFunction()
 }
 
 spvgentwo::Function::~Function()
@@ -33,4 +33,35 @@ void spvgentwo::Function::write(IWriter* _pWriter) const
 
 	Instruction end(m_pAllocator, spv::Op::OpFunctionEnd);
 	end.write(_pWriter);
+}
+
+spvgentwo::Type& spvgentwo::Function::createSignature()
+{
+	return m_Type.Function();
+}
+
+bool spvgentwo::Function::finalize(const Flag<spv::FunctionControlMask> _control)
+{
+	if (m_pModule == nullptr || m_pFunctionType != nullptr)
+	{
+		return false;
+	}
+
+	m_Type.Function();
+
+	// first type is the return type
+	const Instruction* pResultType = m_pModule->addType(m_Type.front());
+
+	// function parameters
+	for (auto it = ++m_Type.begin(), end = m_Type.end(); it != end; ++it) 
+	{
+		m_Parameters.emplace_back(m_pAllocator).opFunctionParameter(m_pModule->addType(*it));
+	}
+
+	// function signature type
+	m_pFunctionType = m_pModule->addType(m_Type);
+
+	m_Function.opFunction(_control, pResultType, m_pFunctionType);
+
+	return true;
 }
