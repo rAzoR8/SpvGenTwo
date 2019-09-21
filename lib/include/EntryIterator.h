569ce007f9b6b803249be6ed69a4d2fd8c2e6231
Fabian Wahlster <fwahlster@outlook.com>
2019-09-21 14:46:38 +0200

    oups fuckedup the staged/unstaged files, heres the iterator changes

--- lib/include/EntryIterator.h
+++ lib/include/EntryIterator.h
@@ -14,6 +14,12 @@ namespace spvgentwo
 		bool operator==(const EntryIterator<T>& _other) const;
 		bool operator!=(const EntryIterator<T>& _other) const;
 
+		bool operator==(nullptr_t) const;
+		bool operator!=(nullptr_t) const;
+
+		EntryIterator<T> operator+(const size_t n) const;
+		EntryIterator<T> operator-(const size_t n) const;
+
 		// pre
 		EntryIterator<T>& operator++();
 		EntryIterator<T>& operator--();
@@ -34,6 +40,8 @@ namespace spvgentwo
 		operator Entry<T>* () { return m_pEntry; }
 		operator const Entry<T>* ()  const { return m_pEntry; }
 
+		//operator bool() const { return m_pEntry != nullptr; }
+
 	private:
 		Entry<T>* m_pEntry = nullptr;
 	};
@@ -48,6 +56,18 @@ namespace spvgentwo
 	inline bool EntryIterator<T>::operator!=(const EntryIterator<T>& _other) const
 	{
 		return m_pEntry != _other.m_pEntry;
+	}
+
+	template<class T>
+	inline bool EntryIterator<T>::operator==(nullptr_t) const
+	{
+		return m_pEntry == nullptr;
+	}
+
+	template<class T>
+	inline bool EntryIterator<T>::operator!=(nullptr_t) const
+	{
+		return m_pEntry != nullptr;
 	}
 
 	template<class T>
@@ -75,6 +95,22 @@ namespace spvgentwo
 	{
 		EntryIterator<T> ret(m_pEntry);
 		m_pEntry = m_pEntry->prev();
+		return ret;
+	}
+
+	template<class T>
+	EntryIterator<T> EntryIterator<T>::operator+(const size_t n) const
+	{
+		EntryIterator<T> ret(m_pEntry);
+		for (size_t i = 0; i < n && ret != nullptr; i++, ++ret) {}
+		return ret;
+	}
+
+	template<class T>
+	EntryIterator<T> EntryIterator<T>::operator-(const size_t n) const
+	{
+		EntryIterator<T> ret(m_pEntry);
+		for (size_t i = 0; i < n && ret != nullptr; i++, --ret) {}
 		return ret;
 	}
 } // !spvgentwo
