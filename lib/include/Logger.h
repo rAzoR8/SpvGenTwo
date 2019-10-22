6dc2163b22e5abec8dc82a77282140c920d8906b
Fabian Wahlster <razor8@gmx.de>
2019-10-22 20:02:15 +0200

    remove helper functions from interface

--- lib/include/Logger.h
+++ lib/include/Logger.h
@@ -18,10 +18,5 @@ namespace spvgentwo
 		virtual ~ILogger();
 
 		virtual void log(const LogLevel _level, const char* _pMsg) = 0;
-		void logDebug(const char* _pMsg) { log(LogLevel::Debug, _pMsg); }
-		void logInfo(const char* _pMsg) { log(LogLevel::Info, _pMsg); }
-		void logWarning(const char* _pMsg) { log(LogLevel::Warning, _pMsg); }
-		void logError(const char* _pMsg) { log(LogLevel::Error, _pMsg); }
-		void logFatal(const char* _pMsg) { log(LogLevel::Fatal, _pMsg); }
 	};
 } //! spvgentwo
