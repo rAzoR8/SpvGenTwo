6522db2f29a1e94a620cf6b50ff01076d1749c25
Fabian Wahlster <fwahlster@outlook.com>
2020-01-24 14:51:53 +0100

    added remove_const trait

--- lib/include/spvgentwo/stdreplacement.h
+++ lib/include/spvgentwo/stdreplacement.h
@@ -114,6 +114,24 @@ namespace spvgentwo::stdrep
 	inline constexpr bool is_same_v<T, T> = true;
 
 	template <class T>
+	struct remove_const { using type = T;};
+
+	template <class T>
+	struct remove_const<const T> {using type = T;};
+
+	template <class T>
+	using remove_const_t = typename remove_const<T>::type;
+
+	template <class T>
+	struct remove_volatile { using type = T; };
+
+	template <class T>
+	struct remove_volatile<volatile T> { using type = T; };
+
+	template <class T>
+	using remove_volatile_t = typename remove_volatile<T>::type;
+
+	template <class T>
 	struct remove_cv { using type = T; };
 	template <class T>
 	struct remove_cv<const T> { using type = T; };
