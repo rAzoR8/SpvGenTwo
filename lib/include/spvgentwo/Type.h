beca6c853469be1cf7b10c1e0f1071765ca79996
Fabian Wahlster <fwahlster@outlook.com>
2020-02-07 19:19:01 +0100

    hasSign checks base type (so it works for vectors as well), add setSign to set a explicit sign to the base type

--- lib/include/spvgentwo/Type.h
+++ lib/include/spvgentwo/Type.h
@@ -289,7 +289,8 @@ namespace spvgentwo
 		bool isComposite() const { return isAggregate() || isMatrix() || isVector(); }
 
 		// check if this type has same sign as _sign
-		bool hasSign(Sign _sign) const { return _sign == Sign::Any || ((_sign == Sign::Signed) == isSInt()); }
+		bool hasSign(Sign _sign) const { return _sign == Sign::Any || ((_sign == Sign::Signed) == getBaseType().isSInt()); }
+		void setSign(bool _signum) { getBaseType().m_IntSign = _signum; }
 
 		bool isOpaque() const { return m_Type == spv::Op::OpTypeOpaque; }
 
