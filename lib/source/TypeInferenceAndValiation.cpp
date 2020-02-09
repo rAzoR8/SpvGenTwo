50a7ced92f6a0bbe51615fcd9b8a9e8ee4c81dcc
Fabian Wahlster <fwahlster@outlook.com>
2020-02-09 11:05:03 +0100

    simplify outer product result type

--- lib/source/TypeInferenceAndValiation.cpp
+++ lib/source/TypeInferenceAndValiation.cpp
@@ -90,10 +90,10 @@ spvgentwo::Instruction* spvgentwo::defaultimpl::inferResultType(const spvgentwo:
 		// Linear - algebraic outer product of Vector 1 and Vector 2.
 		// Result Type must be an OpTypeMatrix whose Column Type is a vector of ﬂoating - point type.
 		// Vector 1 must have the same type as the Column Type in Result Type.
-		// Vector 2 must be a vector with the same Component Type as the Component Type in Result Type.Its number of components must equal the number of columns in Result Type.
-		Type matType(module->getAllocator());
-		matType.MatrixColumn(type2->getVectorComponentCount()).VectorElement(type1->getVectorComponentCount()).Float(type1->front().getFloatWidth());
-		return module->addType(matType);
+		// Vector 2 must be a vector with the same Component Type as the Component Type in Result Type.
+		// Its number of components must equal the number of columns in Result Type.
+
+		return module->addType(type1->wrapMatrix(type2->getVectorComponentCount()));
 	}
 	case spv::Op::OpDot:
 	{
