c4d9ae02b8aecd1a143c8f32103a2cf487d694be
Fabian Wahlster <fwahlster@outlook.com>
2020-03-02 19:14:28 +0100

    add non-const transform variant

--- common/include/common/Algorithms.h
+++ common/include/common/Algorithms.h
@@ -8,10 +8,26 @@ namespace spvgentwo
 	template <class Elem, template <typename T> class Container, class Func>
 	auto transform(const Container<Elem>& _input, const Func& _func)
 	{
-		using Transformed = decltype(stdrep::declval<Func>()(stdrep::declval<const Elem&>));
+		static_assert(traits::is_invocable_v<Func, const Elem&>, "Func not invocable with const Elem&");
+		using Transformed = decltype(stdrep::declval<Func>()(stdrep::declval<const Elem&>()));
 		Container<Transformed> output(_input.getAllocator());
 
 		for(const Elem& elem : _input)
+		{
+			output.emplace_back(_func(elem));
+		}
+
+		return output;
+	}
+
+	template <class Elem, template <typename T> class Container, class Func>
+	auto transform(Container<Elem>& _input, const Func& _func)
+	{
+		static_assert(traits::is_invocable_v<Func, Elem&>, "Func not invocable with Elem&");
+		using Transformed = decltype(stdrep::declval<Func>()(stdrep::declval<Elem&>()));
+		Container<Transformed> output(_input.getAllocator());
+
+		for (Elem& elem : _input)
 		{
 			output.emplace_back(_func(elem));
 		}
