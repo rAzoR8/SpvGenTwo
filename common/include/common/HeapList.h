8a3febe0c547d4a99fe5dca61d5386db4e41269f
Fabian Wahlster <fwahlster@outlook.com>
2020-03-20 21:19:14 +0100

    add heaplist

---
+++ common/include/common/HeapList.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "spvgentwo/List.h"
+#include "HeapAllocator.h"
+
+namespace spvgentwo
+{
+	template <class T>
+	class HeapList : List<T>
+	{
+	public:
+		using List<T>::List;
+
+		HeapList() : List<T>(HeapAllocator::instance()) {}
+
+		HeapList(const HeapList& _other) : List<T>(_other) {}
+
+		HeapList(HeapList&& _other) : List<T>(stdrep::move(_other)) {}
+
+		template <class ...Args> // args must be of type T
+		HeapList(T&& _first, Args&& ... _args) : List<T>(HeapAllocator::instance(), stdrep::forward<T>(_first), stdrep::forward<Args>(_args)...) {}
+
+		HeapList& operator=(const HeapList<T>& _other) { List<T>::operator=(_other); return *this; }
+		HeapList& operator=(HeapList<T>&& _other) noexcept { List<T>::operator=(stdrep::move(_other)); return *this; }
+
+		virtual ~HeapList() override = default;
+	};
+} 
