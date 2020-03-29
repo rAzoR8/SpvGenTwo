c083620e27964256989a94edd8ec752207cc6908
Fabian Wahlster <fwahlster@outlook.com>
2020-03-29 22:38:06 +0200

    add binary writer for vector-like containers

--- common/include/common/BinaryFileWriter.h
+++ common/include/common/BinaryFileWriter.h
@@ -10,8 +10,8 @@ namespace spvgentwo
 	public:
 		BinaryFileWriter(const char* _path);
 		~BinaryFileWriter();
-		long put(unsigned int _word);
-		void putAt(unsigned int _word, const long _offset);
+		long put(unsigned int _word) final;
+		void putAt(unsigned int _word, const long _offset) final;
 
 	private:
 		FILE* m_pFile = nullptr;
