b968529847cf24300e601c09bdc6edb73cc5cb4f
Fabian Wahlster <fwahlster@outlook.com>
2020-04-04 18:47:36 +0200

    fix vector writer

--- common/include/common/BinaryVectorWriter.h
+++ common/include/common/BinaryVectorWriter.h
@@ -11,29 +11,15 @@ namespace spvgentwo
 		BinaryVectorWriter(U32Vector& _vector) : m_vector(_vector) {};
 		virtual ~BinaryVectorWriter() {};
 
-		long put(unsigned int _word) final;
-		void putAt(unsigned int _word, const long _offset) final;
+		void put(unsigned int _word) final;
 
 	private:
 		U32Vector& m_vector;
 	};
 
 	template<typename U32Vector>
-	inline long BinaryVectorWriter<U32Vector>::put(unsigned int _word)
+	inline void BinaryVectorWriter<U32Vector>::put(unsigned int _word)
 	{
 		m_vector.emplace_back(_word);
-		return static_cast<long>(m_vector.size() * sizeof(unsigned int));
-	}
-
-	template<typename U32Vector>
-	inline void BinaryVectorWriter<U32Vector>::putAt(unsigned int _word, const long _offset)
-	{
-		const auto index = _offset / sizeof(unsigned int);
-		if (m_vector.size() <= index)
-		{
-			m_vector.resize(index + 1u);
-		}
-
-		m_vector[index] = _word;
 	}
 } //!spvgentwo
