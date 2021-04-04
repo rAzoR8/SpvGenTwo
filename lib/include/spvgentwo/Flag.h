b4471ce13ff933b2d9fc81ba38ee4f56c3e7f155
Fabian Wahlster <fwahlster@outlook.com>
2021-04-04 20:50:45 +0300

    rename flagXXX functions, add default Flag constructor

--- lib/include/spvgentwo/Flag.h
+++ lib/include/spvgentwo/Flag.h
@@ -28,17 +28,19 @@ namespace spvgentwo
 	{
 		static_assert(sizeof(Enum) <= sizeof(unsigned int), "Enum type does not fit into mask");
 
+		constexpr Flag() : mask{ 0u } {}
+
 		template <class ... TEnum>
 		constexpr Flag(Enum _first, TEnum ... _enums) : mask{ detail::bit_or(_first, _enums...) } { }
 
 		template <class ... TEnum>
-		static constexpr Flag flagOR(Enum _first, TEnum ... _enums) { return static_cast<Enum>(detail::bit_or(_first, _enums...)); }
+		static constexpr Flag OR(Enum _first, TEnum ... _enums) { return static_cast<Enum>(detail::bit_or(_first, _enums...)); }
 
 		template <class ... TEnum>
-		static constexpr Flag flagXOR(Enum _first, TEnum ... _enums) { return static_cast<Enum>(detail::bit_xor(_first, _enums...)); }
+		static constexpr Flag XOR(Enum _first, TEnum ... _enums) { return static_cast<Enum>(detail::bit_xor(_first, _enums...)); }
 
 		template <class ... TEnum>
-		static constexpr Flag flagAND(Enum _first, TEnum ... _enums) { return static_cast<Enum>(detail::bit_and(_first, _enums...)); }
+		static constexpr Flag AND(Enum _first, TEnum ... _enums) { return static_cast<Enum>(detail::bit_and(_first, _enums...)); }
 
 		template <class ... TEnum>
 		void set(Enum _first, TEnum ... _enums)
@@ -63,16 +65,16 @@ namespace spvgentwo
 	};
 
 	template<class Enum>
-	constexpr Flag<Enum> operator|(const Flag<Enum>& l, const Flag<Enum>& r) { return Flag<Enum>::flagOR(l, r); }
+	constexpr Flag<Enum> operator|(const Flag<Enum>& l, const Flag<Enum>& r) { return Flag<Enum>::OR(l, r); }
 	template<class Enum>
-	constexpr Flag<Enum> operator&(const Flag<Enum>& l, const Flag<Enum>& r) { return Flag<Enum>::flagAND(l, r); }
+	constexpr Flag<Enum> operator&(const Flag<Enum>& l, const Flag<Enum>& r) { return Flag<Enum>::AND(l, r); }
 	template<class Enum>
-	constexpr Flag<Enum> operator^(const Flag<Enum>& l, const Flag<Enum>& r) { return Flag<Enum>::flagXOR(l, r); }
+	constexpr Flag<Enum> operator^(const Flag<Enum>& l, const Flag<Enum>& r) { return Flag<Enum>::XOR(l, r); }
 
 	template<class Enum>
-	constexpr Flag<Enum> operator|(const Flag<Enum>& l, const Enum& r) { return Flag<Enum>::flagOR(l, r); }
+	constexpr Flag<Enum> operator|(const Flag<Enum>& l, const Enum& r) { return Flag<Enum>::OR(l, r); }
 	template<class Enum>
-	constexpr Flag<Enum> operator&(const Flag<Enum>& l, const Enum& r) { return Flag<Enum>::flagAND(l, r); }
+	constexpr Flag<Enum> operator&(const Flag<Enum>& l, const Enum& r) { return Flag<Enum>::AND(l, r); }
 	template<class Enum>
-	constexpr Flag<Enum> operator^(const Flag<Enum>& l, const Enum& r) { return Flag<Enum>::flagXOR(l, r); }
+	constexpr Flag<Enum> operator^(const Flag<Enum>& l, const Enum& r) { return Flag<Enum>::XOR(l, r); }
 } // !spvgentwo
