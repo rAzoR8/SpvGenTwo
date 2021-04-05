580f29a6e842c8c429606674f3e90b971860987f
Fabian Wahlster <fwahlster@outlook.com>
2021-04-05 14:47:22 +0300

    HashMapIterator constexpr

--- lib/include/spvgentwo/HashMapIterator.h
+++ lib/include/spvgentwo/HashMapIterator.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "List.h"
-#include "Hasher.h"
+#include "FNV1aHasher.h"
 
 namespace spvgentwo
 {
@@ -12,7 +12,7 @@ namespace spvgentwo
 		friend class HashMap;
 
 		template <class ...Args>
-		NodeT(Args&& ... _args) : kv{ stdrep::forward<Args>(_args)... }, hash{ 0u }{}
+		constexpr NodeT(Args&& ... _args) : kv{ stdrep::forward<Args>(_args)... }, hash{ 0u }{}
 
 		struct KV
 		{
@@ -21,7 +21,7 @@ namespace spvgentwo
 		} kv;
 
 	private:
-		Hash64 hash;
+		Hash64 hash{};
 	};
 
 	template <class Key, class Value>
@@ -35,29 +35,29 @@ namespace spvgentwo
 		using KeyValue = typename Node::KV;
 		using Bucket = List<Node>;
 
-		HashMapIterator(Bucket* _pBucket = nullptr, Bucket* _pEnd = nullptr, typename Bucket::Iterator _element = nullptr) : m_element(_element), m_pBucket(_pBucket), m_pEnd(_pEnd){}
-		HashMapIterator(const HashMapIterator& _other) : m_element(_other.m_element), m_pBucket(_other.m_pBucket), m_pEnd(_other.m_pEnd) {}
+		constexpr HashMapIterator(Bucket* _pBucket = nullptr, Bucket* _pEnd = nullptr, typename Bucket::Iterator _element = nullptr) : m_element(_element), m_pBucket(_pBucket), m_pEnd(_pEnd){}
+		constexpr HashMapIterator(const HashMapIterator& _other) : m_element(_other.m_element), m_pBucket(_other.m_pBucket), m_pEnd(_other.m_pEnd) {}
 
-		bool operator==(const HashMapIterator& _other) const;
-		bool operator!=(const HashMapIterator& _other) const;
+		constexpr bool operator==(const HashMapIterator& _other) const;
+		constexpr bool operator!=(const HashMapIterator& _other) const;
 
-		bool operator==(sgt_nullptr_t) const;
-		bool operator!=(sgt_nullptr_t) const;
+		constexpr bool operator==(sgt_nullptr_t) const;
+		constexpr bool operator!=(sgt_nullptr_t) const;
 
 		// pre
-		HashMapIterator<Key, Value>& operator++();
+		constexpr HashMapIterator<Key, Value>& operator++();
 
 		// post
-		HashMapIterator<Key, Value> operator++(int);
+		constexpr HashMapIterator<Key, Value> operator++(int);
 
-		KeyValue& operator*() { return m_element->kv; }
-		const KeyValue& operator*() const { return m_element->kv; }
+		constexpr KeyValue& operator*() { return m_element->kv; }
+		constexpr const KeyValue& operator*() const { return m_element->kv; }
 
-		KeyValue* operator->() { return &m_element->kv; }
-		const KeyValue* operator->() const { return &m_element->kv; }
+		constexpr KeyValue* operator->() { return &m_element->kv; }
+		constexpr const KeyValue* operator->() const { return &m_element->kv; }
 
 		// check if iterator is valid, can be derefed. Might still be at end!
-		operator bool() const { return m_pBucket != nullptr || m_element == nullptr; }
+		constexpr operator bool() const { return m_pBucket != nullptr || m_element == nullptr; }
 
 	private:
 		typename Bucket::Iterator m_element = nullptr;
@@ -66,31 +66,31 @@ namespace spvgentwo
 	};
 
 	template<class Key, class Value>
-	inline bool HashMapIterator<Key, Value>::operator==(const HashMapIterator& _other) const
+	inline constexpr bool HashMapIterator<Key, Value>::operator==(const HashMapIterator& _other) const
 	{
 		return m_pBucket == _other.m_pBucket && m_pEnd == _other.m_pEnd && m_element == _other.m_element;;
 	}
 
 	template<class Key, class Value>
-	inline bool HashMapIterator<Key, Value>::operator!=(const HashMapIterator& _other) const
+	inline constexpr bool HashMapIterator<Key, Value>::operator!=(const HashMapIterator& _other) const
 	{
 		return m_pBucket != _other.m_pBucket || m_pEnd != _other.m_pEnd || m_element != _other.m_element;;
 	}
 
 	template<class Key, class Value>
-	inline bool HashMapIterator<Key, Value>::operator==(sgt_nullptr_t) const
+	inline constexpr bool HashMapIterator<Key, Value>::operator==(sgt_nullptr_t) const
 	{
 		return m_pBucket == nullptr || m_element == nullptr;
 	}
 
 	template<class Key, class Value>
-	inline bool HashMapIterator<Key, Value>::operator!=(sgt_nullptr_t) const
+	inline constexpr bool HashMapIterator<Key, Value>::operator!=(sgt_nullptr_t) const
 	{
 		return m_pBucket != nullptr && m_element != nullptr;
 	}
 
 	template<class Key, class Value>
-	inline HashMapIterator<Key, Value>& HashMapIterator<Key, Value>::operator++()
+	inline constexpr HashMapIterator<Key, Value>& HashMapIterator<Key, Value>::operator++()
 	{
 		if (++m_element == nullptr)
 		{
@@ -108,7 +108,7 @@ namespace spvgentwo
 	}
 
 	template<class Key, class Value>
-	inline HashMapIterator<Key, Value> HashMapIterator<Key, Value>::operator++(int)
+	inline constexpr HashMapIterator<Key, Value> HashMapIterator<Key, Value>::operator++(int)
 	{
 		HashMapIterator<Key, Value> ret(*this);
 		this->operator++();
