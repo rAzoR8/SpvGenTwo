a80858247cf2affdf28f79eda373a4c3196e5022
Fabian Wahlster <fwahlster@outlook.com>
2021-05-13 21:36:25 +0300

    refactor linker CLI to be more extensible & allow printing minimal help

--- link/source/link.cpp
+++ link/source/link.cpp
@@ -32,6 +32,17 @@ struct Target
 	bool exportAllReferencedVars = false; // for instr == OpFunction export
 };
 
+const char* g_out = nullptr;
+const char* g_patchspv = nullptr;
+const char* g_targetspv = nullptr;
+
+HeapList<Target> g_targets;
+HeapList<Module> g_libs;
+LinkerHelper::LinkerOptions g_options{};
+
+Module g_patchModule(&g_alloc, &g_logger);
+Module g_targetModule(&g_alloc, &g_logger);
+
 int patch(Module& _module, const HeapList<Target>& _targets, const char* _out)
 {
 	_module.addCapability(spv::Capability::Linkage);
@@ -73,7 +84,7 @@ int patch(Module& _module, const HeapList<Target>& _targets, const char* _out)
 	BinaryFileWriter writer(_out);
 	if (writer.isOpen() == false)
 	{
-		g_logger.logError("Failed to open %s", _out);
+		g_logger.logError("Failed to open \'%s\'", _out);
 		return -1;
 	}
 
@@ -94,7 +105,7 @@ int link(HeapList<Module>& _libs, Module& _target, const LinkerHelper::LinkerOpt
 	BinaryFileWriter writer(_out);
 	if (writer.isOpen() == false)
 	{
-		g_logger.logError("Failed to open %s", _out);
+		g_logger.logError("Failed to open \'%s\'", _out);
 		return -1;
 	}
 
@@ -103,26 +114,63 @@ int link(HeapList<Module>& _libs, Module& _target, const LinkerHelper::LinkerOpt
 	// don't need to call assignIDs, we are using AssignResultIDs
 	if (_target.write(writer) == false) 
 	{
-		g_logger.logError("Failed to write %s", _out);
+		g_logger.logError("Failed to write \'%s\'", _out);
 		return -1;
 	}
 
 	return 0;
 }
 
+void addTarget(spv::LinkageType type, int& _i, int _argc, char* _argv[])
+{
+	Instruction* instr = nullptr;
+	const char* target = _argv[++_i];
+	if (auto id = strtoul(target, nullptr, 10); id != 0u && id != sgt_uint32_max)
+	{
+		instr = g_patchModule.getInstructionById(spv::Id{ id });
+	}
+	else
+	{
+		instr = g_patchModule.getInstructionByName(target);
+	}
+
+	if (instr == nullptr)
+	{
+		g_logger.logError("Could not find %s in %s", target, g_patchspv);
+		return;
+	}
+
+	if (const char* name = _argv[++_i]; name != nullptr)
+	{
+		bool exportVars = false;
+		if (type == spv::LinkageType::Export && _i + 1 < _argc && (strcmp(_argv[_i + 1], "--vars") == 0))
+		{
+			++_i;
+			exportVars = true;
+		}
+
+		g_targets.emplace_back(instr, name, type, exportVars);
+		g_logger.logInfo("Adding %s target instruction \'%s\' with name \'%s\'", type == spv::LinkageType::Import ? "import" : "export", target, name);
+	}
+}
+
 int main(int argc, char* argv[])
 {
+	g_logger.logInfo("SpvGenTwoLinker by Fabian Wahlster - https://github.com/rAzoR8/SpvGenTwo");
+
 	using namespace LinkerHelper;
 
 	struct CMDOption
 	{
-		using Func = void(LinkerOptions& _options);
-		const char* str;
-		const char* alt;
+		using Func = bool(int& i, int _argc, char* _argv[]);
+		const char* str = nullptr;
+		const char* alt = nullptr;
 		Func* func = nullptr;
+		int args = 0;
 	};
 
-#define FLAGOPTION(x, acronym) {"--" #x, "--"#acronym, [](LinkerOptions& _options){_options.flags |= LinkerOptionBits::x;}}
+	
+#define FLAGOPTION(x, acronym) {"--" #x, "--"#acronym, []([[maybe_unused]] int& _i, [[maybe_unused]] int _argc, [[maybe_unused]] char* _argv[]){g_options.flags |= LinkerOptionBits::x; g_logger.logInfo("Option: " #x); return true;}}
 
 	const CMDOption CmdOptions[] =
 	{
@@ -140,162 +188,156 @@ int main(int argc, char* argv[])
 		FLAGOPTION(CopyOpLineInstructions, lines),
 		FLAGOPTION(CopyOpModuleProcessedInstructions, processed),
 		{
-			"--PrintInstructions",
-			"--verbose",
-			[](LinkerOptions& _options){
-			_options.grammar = &g_gram;
-			_options.printer = &g_printer;
+			"--AllOptions", "--auto",
+			[]([[maybe_unused]] int& _i, [[maybe_unused]] int _argc, [[maybe_unused]] char* _argv[]) {
+				g_options.flags |= LinkerOptionBits::All; g_logger.logInfo("Option: AllOptions"); return true;
 			}
+		},
+		{ // verbose
+			"--PrintInstructions", "--verbose",
+			[]([[maybe_unused]] int& _i, [[maybe_unused]] int _argc, [[maybe_unused]] char* _argv[]){
+				g_options.grammar = &g_gram;
+				g_options.printer = &g_printer;
+				g_logger.logInfo("Verbose instruction printing enabled");
+				return true;
+			}
+		},
+		{ // patch spv
+			"--patchspv", "--p",
+			[](int& _i, [[maybe_unused]] int _argc, char* _argv[]) {
+				g_patchspv = _argv[++_i];
+				BinaryFileReader reader(g_alloc, g_patchspv);
+				if (reader == false)
+				{
+					g_logger.logError("Failed to open \'%s\'", g_patchspv);
+					return false;
+				}
+				else if (g_patchModule.readAndInit(reader, g_gram) == false)
+				{
+					return false;
+				}
+				g_logger.logInfo("Loaded patch target \'%s\'", g_patchspv);
+				return true;
+			},
+			1 // one additonal path arg
+		},
+		{ // lib module
+			"--lib", "--l",
+			[](int& _i, [[maybe_unused]] int _argc, char* _argv[]) {
+				const char* file = _argv[++_i];
+				BinaryFileReader reader(g_alloc, file);
+				if (reader == false)
+				{
+					g_logger.logError("Failed to open \'%s\'", file);
+					return false;
+				}
+				else if (g_libs.emplace_back(&g_alloc, &g_logger).readAndInit(reader, g_gram) == false)
+				{
+					return false;
+				}
+				g_logger.logInfo("Loaded lib \'%s\'", file);
+				return true;
+			},
+			1 // one additonal path arg
+		},
+		{ // target/consumer module
+			"--target", "--t",
+			[](int& _i, [[maybe_unused]] int _argc, char* _argv[]) {
+				g_targetspv = _argv[++_i];
+				BinaryFileReader reader(g_alloc, g_targetspv);
+				if (reader == false)
+				{
+					g_logger.logError("Failed to open \'%s\'", g_targetspv);
+					return false;
+				}
+				else if (g_targetModule.readAndInit(reader, g_gram) == false)
+				{
+					return false;
+				}
+				g_logger.logInfo("Loaded target/consumer \'%s\'", g_targetspv);
+				return true;
+			},
+			1 // one additonal path arg
+		},
+		{ // (link) output  module
+			"--out", "--o",
+			[](int& _i, [[maybe_unused]] int _argc, char* _argv[]) {
+				g_out = _argv[++_i];
+				g_logger.logInfo("Output file \'%s\'", g_out);
+				return true;
+			},
+			1 // one additonal path arg
+		},
+		{ // import symbol
+			"--import", "--i",
+			[](int& _i, int _argc, char* _argv[]) {
+				addTarget(spv::LinkageType::Import, _i, _argc, _argv);
+				return true;
+			},
+			2 // symbol name and export name
+		},
+		{ // import symbol
+			"--export", "--e",
+			[](int& _i, int _argc, char* _argv[]) {
+				addTarget(spv::LinkageType::Export, _i, _argc, _argv);
+				return true;
+			},
+			2 // symbol name and export name
 		}
 	};
 
-	const char* out = nullptr;
-	const char* patchspv = nullptr;
-	const char* targetspv = nullptr;
-	
-	HeapList<Target> targets;
-	HeapList<Module> libs;
-	LinkerOptions options{};
-
-	options.flags = LinkerOptionBits::AssignResultIDs;
+	g_options.flags = LinkerOptionBits::AssignResultIDs;
 	// we dont want to set LinkerOptionBits::UpdateEntryPointGlobalVarInterface, we call finalizeEntryPoints() manually 
 
-	auto addOption = [&options, &CmdOptions](const char* arg) -> bool
+	auto checkParam = [argc, argv, &CmdOptions](const char* arg, int& i) -> bool
 	{
 		auto casecmp = [](const char* l, const char* r)
 		{
-			while(*l != 0 && *r != 0)
+			while (*l != 0 && *r != 0)
 			{
 				if (tolower(*l) != tolower(*r))
 					return false;
 				++l; ++r;
 			}
-			return true;
+			return *l == 0 && *r == 0;
 		};
-		for(const CMDOption& o : CmdOptions)
+		for (const CMDOption& o : CmdOptions)
 		{
-			if(casecmp(arg, o.alt) || casecmp(arg, o.str))
+			if (casecmp(arg, o.alt) || casecmp(arg, o.str))
 			{
-				g_logger.logInfo("Option: %s", arg);
-				o.func(options);
-				return true;
+				if (i + o.args < argc)
+				{
+					return o.func(i, argc, argv);				
+				}
+				g_logger.logError("Insufficient trailing arguments: i %i + args %i >= argc %i", i, o.args, argc);
+				return false;
 			}
 		}
+		g_logger.logWarning("Unkown parameter %s", arg);
 		return false;
 	};
 
-	Module patchModule(&g_alloc, &g_logger);
-	Module targetModule(&g_alloc, &g_logger);
-
-	int i = 1u;
-	auto addTarget = [&](spv::LinkageType type)
+	for (int i = 1; i < argc; ++i)
 	{
-		Instruction* instr = nullptr;
-		const char* target = argv[++i];
-		if (auto id = strtoul(target, nullptr, 10); id != 0u && id != sgt_uint32_max)
-		{
-			instr = patchModule.getInstructionById(spv::Id{ id });
-		}
-		else
-		{
-			instr = patchModule.getInstructionByName(target);
-		}
-
-		if (instr == nullptr)
-		{
-			g_logger.logError("Could not find %s in %s", target, patchspv);
-			return;
-		}
-
-		if (const char* name = argv[++i]; name != nullptr)
-		{
-			bool exportVars = false;
-			if (type == spv::LinkageType::Export && i + 1 < argc && (strcmp(argv[i + 1], "--vars") == 0))
-			{
-				++i;
-				exportVars = true;
-			}
-
-			targets.emplace_back(instr, name, type, exportVars);
-			g_logger.logInfo("Adding %s target instruction \'%s\' with name \'%s\'", type == spv::LinkageType::Import ? "import" : "export", target, name);
-		}
-	};
-
-	for (; i < argc; ++i)
-	{
-		const char* arg = argv[i];
-		if (i + 1 < argc && (strcmp(arg, "--p") == 0 || strcmp(arg, "--patchspv") == 0))
-		{
-			patchspv = argv[++i];
-			BinaryFileReader reader(g_alloc, patchspv);
-			if (reader == false)
-			{
-				g_logger.logError("Failed to open %s", patchspv);
-				return -1;
-			}
-			else if (patchModule.readAndInit(reader, g_gram) == false)
-			{
-				return -1;
-			}
-		}
-		else if (i + 1 < argc && (strcmp(arg, "--l") == 0 || strcmp(arg, "--lib") == 0))
-		{
-			const char* file = argv[++i];
-			BinaryFileReader reader(g_alloc, file);
-			if (reader == false)
-			{
-				g_logger.logError("Failed to open %s", file);
-				return -1;
-			}
-			else if (libs.emplace_back(&g_alloc, &g_logger).readAndInit(reader, g_gram) == false)
-			{
-				return -1;
-			}
-		}
-		else if (i + 1 < argc && (strcmp(arg, "--t") == 0 || strcmp(arg, "--target") == 0))
-		{
-			targetspv = argv[++i];
-			BinaryFileReader reader(g_alloc, targetspv);
-			if (reader == false)
-			{
-				g_logger.logError("Failed to open %s", targetspv);
-				return -1;
-			}
-			else if (targetModule.readAndInit(reader, g_gram) == false)
-			{
-				return -1;
-			}
-		}
-		else if (i + 1 < argc && (strcmp(arg, "--o") == 0 || strcmp(arg, "--out") == 0))
-		{
-			out = argv[++i];
-			g_logger.logInfo("Output file \'%s\'", out);
-		}
-		else if (i + 1 < argc && (strcmp(arg, "--e") == 0 || strcmp(arg, "--export") == 0))
-		{
-			addTarget(spv::LinkageType::Export);
-		}
-		else if (i + 1 < argc && (strcmp(arg, "--i") == 0 || strcmp(arg, "--import") == 0))
-		{
-			addTarget(spv::LinkageType::Import);
-		}
-		else
-		{
-			if (addOption(arg) == false)
-			{
-				g_logger.logWarning("Unkown parameter %s", arg);
-			}
-		}
+		checkParam(argv[i], i);
 	}
 
-	if (targets.empty() == false)
+	if (g_targets.empty() == false)
 	{
-		return patch(patchModule, targets, out != nullptr ? out : patchspv);
+		return patch(g_patchModule, g_targets, g_out != nullptr ? g_out : g_patchspv);
 	}
 
-	if (libs.empty() == false)
+	if (g_libs.empty() == false)
 	{
-		return link(libs, targetModule, options, out != nullptr ? out : targetspv);
+		return link(g_libs, g_targetModule, g_options, g_out != nullptr ? g_out : g_targetspv);
+	}
+
+	if (argc == 1 || (g_libs.empty() && g_targets.empty())) // print 'help'
+	{
+		for (const CMDOption& o : CmdOptions)
+		{
+			g_logger.logInfo("Option %s or %s with %i args", o.str, o.alt, o.args);
+		}
 	}
 
 	return 0;
