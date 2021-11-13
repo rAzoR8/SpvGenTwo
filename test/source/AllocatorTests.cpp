1726c8078a7532a3a7c2158b00cbc363395f69de
Fabian Wahlster <fwahlster@outlook.com>
2021-11-13 16:41:42 +0200

    add console reporter

--- test/source/AllocatorTests.cpp
+++ test/source/AllocatorTests.cpp
@@ -1,5 +1,11 @@
 #include <catch2/catch_test_macros.hpp>
 
+#include <catch2/reporters/catch_reporter_console.h>
+namespace Catch
+{
+	CATCH_REGISTER_REPORTER("console", ConsoleReporter)
+}
+
 static int Factorial( int number ) {
    return number <= 1 ? number : Factorial( number - 1 ) * number;  // fail
 // return number <= 1 ? 1      : Factorial( number - 1 ) * number;  // pass
