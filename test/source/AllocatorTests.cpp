cd3b2ab4dcd006e8c3cdbccd27cf6d50479c4570
Fabian Wahlster <fwahlster@outlook.com>
2022-04-03 12:51:38 +0300

    migrate to catch2 preview4

--- test/source/AllocatorTests.cpp
+++ test/source/AllocatorTests.cpp
@@ -1,6 +1,5 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/reporters/catch_reporter_console.hpp>
-#include <catch2/catch_reporter_registrars.hpp>
 
 #include "common/HeapAllocator.h"
 using namespace spvgentwo;
