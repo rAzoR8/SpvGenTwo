002b83b6457d6be4d3862a90adeff01fa85ea34c
Fabian Wahlster <fwahlster@outlook.com>
2022-04-12 21:12:54 +0300

    more alloc tests

--- test/source/AllocatorTests.cpp
+++ test/source/AllocatorTests.cpp
@@ -9,4 +9,20 @@ TEST_CASE( "alignment", "[HeapAllocator]" ) {
 
     // not a power of two, must fail
     REQUIRE( alloc.allocate( 10u, 3 ) == nullptr );
+
+    auto test = [&alloc](unsigned bytes, unsigned align)
+    {
+        void* p = alloc.allocate(10u, 4);
+        REQUIRE(p != nullptr);
+        REQUIRE((sgt_uint64_t)p % align == 0);
+        alloc.deallocate(p, bytes);
+    };
+
+    test(1, 1);
+    test(10, 1);
+    test(10, 4);
+    test(10, 8);
+    test(16, 16);
+    test(16, 16);
+    test(1337, 1);
 }
